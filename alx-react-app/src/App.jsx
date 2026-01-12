0fd41e32d6c5955e117c9fbebf3182832ee89e60
Yazouzou <azouzou.yousra@gmail.com>
2026-01-12 22:11:42 +0100

    Update App.jsx
    Integrate WelcomeMessage component into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,9 @@
-import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
+
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <Footer />
+      <WelcomeMessage />
     </div>
   );
 }
