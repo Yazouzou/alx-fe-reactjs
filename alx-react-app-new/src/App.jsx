405129e08c57adadab5ce703f689649957be6acb
Yazouzou <azouzou.yousra@gmail.com>
2026-01-18 21:17:13 +0100

    Enhance React components with inline CSS styling

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
+// App.jsx
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <UserProfile name="Yousra" age={29} bio="I love traveling and coding!" />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
