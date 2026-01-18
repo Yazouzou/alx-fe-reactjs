6f3e0cf5e2d88765da3ca9362f8cca3f5d4f08a6
Yazouzou <azouzou.yousra@gmail.com>
2026-01-18 23:50:54 +0100

    Add WelcomeMessage component with updated JSX text

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile"; // if used
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
