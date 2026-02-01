43eb633a5935871af662d450b49be6ce51dcbed1
Yazouzou <azouzou.yousra@gmail.com>
2026-02-01 22:30:55 +0100

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,13 @@
+import Search from "./components/Search";
 function App() {
   console.log(import.meta.env.VITE_APP_GITHUB_API_KEY);
 
-  return <h1>GitHub User Search</h1>;
+  return (
+      <div>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
 }
 
 export default App;
--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,11 @@
+import Search from "./components/Search";
+
 function App() {
   console.log(import.meta.env.VITE_APP_GITHUB_API_KEY);
-
+  <div>
   return <h1>GitHub User Search</h1>;
+  <Search />
+  </div>
 }
 
 export default App;
