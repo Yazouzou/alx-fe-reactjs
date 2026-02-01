07ea52a96c4443f265458020e0d0b946b88984ae
Yazouzou <azouzou.yousra@gmail.com>
2026-02-01 22:00:15 +0100

    Update App.jsx
    add import syntax

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,7 @@
 function App() {
-  return (
-    <div>
-      <h1>GitHub User Search</h1>
-    </div>
-  );
+  console.log(import.meta.env.VITE_APP_GITHUB_API_KEY);
+
+  return <h1>GitHub User Search</h1>;
 }
 
 export default App;
