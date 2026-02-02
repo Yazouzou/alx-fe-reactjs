a110f7276d1c391bf14284c0360cfdaeb2f82dda
Yazouzou <azouzou.yousra@gmail.com>
2026-02-02 02:24:42 +0100

    Adding the tailwind component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,8 +3,11 @@ function App() {
   console.log(import.meta.env.VITE_APP_GITHUB_API_KEY);
 
   return (
-      <div>
-      <h1>GitHub User Search</h1>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1>GitHub User Search
+        className="text-4xl font-bold text-green-400"
+        Tailwind is working 🚀
+      </h1>
       <Search />
     </div>
   );
