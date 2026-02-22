fe1d7850298b3ed9d36f354f434149e8bd93aad0
Yazouzou <azouzou.yousra@gmail.com>
2026-02-22 22:37:25 +0000

    Add react-router-advanced project with nested, dynamic, and protected routes

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const isLoggedIn = false; 
+
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Sample Blog</Link>
+      </nav>
+
+      <Routes>
+  <Route
+    path="/profile/*"
+    element={
+      <ProtectedRoute isAuthenticated={isLoggedIn}>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
